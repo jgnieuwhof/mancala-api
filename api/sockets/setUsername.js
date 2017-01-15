d35a6811e4040581d0c3e640ea385beeb5b1ba97
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2017-01-15 10:26:04 -0500

    Added server side username validation

--- api/sockets/setUsername.js
+++ api/sockets/setUsername.js
@@ -4,14 +4,15 @@ import chalk from 'chalk'
 
 export default ({ socket }) => {
   socket.on(`client::setUsername`, async ({ username }) => {
+    if (!username || username.length < 6 || username.length > 20)
+      console.log(chalk.red(`invalid username: ${username}`))
+
     try {
-      if (username) {
-        global.state = u({
-          users: {
-            [socket.id] : { username },
-          },
-        }, global.state)
-      }
+      global.state = u({
+        users: {
+          [socket.id] : { username },
+        },
+      }, global.state)
       console.log(JSON.stringify(global.state, null, 4))
       console.log(chalk.green(
         `${socket.id} just set their username to ${username}!`
